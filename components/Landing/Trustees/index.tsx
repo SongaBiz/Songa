75ba89032496923c47d9e51ea9155b97d5bd9624
Evans Mungai <thetracker254@gmail.com>
2024-02-22 14:01:04 +0300

    Refactor code in testimonials section

--- components/Landing/Trustees/index.tsx
+++ components/Landing/Trustees/index.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export const Trustees = () => {
     return (
-        <div className="lg:py-6 md:py-4 lg:px-12 md:px-3 flex flex-1 items-center shadow-2xl shadow-gray-200 border-solid relative w-screen">
-            <h2 className="text-[#A90000] font-medium font-[35px] leading-[45.5px]">Trusted by</h2>
+        <div className="lg:py-6 md:py-4 lg:px-12 md:px-3 flex flex-1 items-center shadow-2xl shadow-gray-200 border-solid space-x-9 relative w-screen">
+            <h2 className="text-[#A90000] font-medium text-[35px] leading-[45.5px]">Trusted by</h2>
             <div className="flex flex-1 items-center place-content-evenly">
                 <Image
                     src='/assets/images/Eastmatt.jpg'
