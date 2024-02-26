2a0e7daec798fe52177159bc64377c40bbd57ff0
Evans Mungai <thetracker254@gmail.com>
2024-02-26 15:37:10 +0300

    Change landing footer size

--- components/Landing/LandingFooter/index.tsx
+++ components/Landing/LandingFooter/index.tsx
@@ -2,7 +2,7 @@
 
 export const LandingFooter = ()=>{
     return(
-        <div className="bg-white py-20 mb-10 px-16">
+        <div className="bg-white py-10 mb-5 px-16">
             <p className="font-[24px] leading-[31.2px]">Visit us at dfnvjkffgfj near bfbdh</p>
             <h3 className="font-[24px] leading-[31.2px] text-[#A90000]"><span className="text-bold text-black">Phone:</span> +254 123 456 789</h3>
             <h3 className="font-[24px] leading-[31.2px] text-[#A90000]"><span className="text-bold text-black">Email:</span> ogollapeter@gmail.com</h3>
