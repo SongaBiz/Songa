e781df0305b04cc32324c5462f62d15d595fab99
Evans Mungai <thetracker254@gmail.com>
2024-02-26 15:30:01 +0300

    Finish adding learn more page components

--- app/learn-more/page.tsx
+++ app/learn-more/page.tsx
@@ -3,7 +3,7 @@ import Cards from "@/components/Landing/Cards";
 
 export default function LearnMore() {
     return (
-        <div className='relative h-[100vh] w-screen'>
+        <div className='relative h-[700px] max-h-full w-screen'>
             <div className='w-full '>
                 <Image
                     src='/assets/images/about-bg.jpg'
@@ -12,7 +12,7 @@ export default function LearnMore() {
                     className='hidden lg:block'
                 />
             </div>
-            <div className="w-full h-[100vh] flex flex-col bg-white bg-opacity-60 absolute z-0 py-5 px-[125px]">
+            <div className="w-full flex flex-col bg-green lg:bg-white lg:bg-opacity-80 absolute z-0 py-5 px-[125px]">
                 <Image
                     src='/assets/images/miniflag.png'
                     alt='boba boda riders'
@@ -29,11 +29,35 @@ export default function LearnMore() {
                         alt='boba boda riders'
                         width={490}
                         height={368}
-                        className='object-cover md:object-fill opacity-80'
+                        className='hidden lg:block object-cover md:object-fill opacity-80'
                     />
                 </div>
-                <div className="flex flex-1 -mt-10">
-                    <Cards />
+                <div className="flex flex-1 lg:-mt-10">
+                    {/* <Cards /> */}
+                    <div className='w-screen h-auto px-4'>
+                        <div className='mx-auto px-6 md:pt-8 md:grid grid-cols-3 gap-8'>
+                            <div className='w-[350px] mx-auto md:w-auto bg-white md:bg-transparent shadow-2xl flex flex-col p-4 my-8 rounded-lg hover:scale-105 duration-300 items-center'>
+                                <Image className='w-20 mx-4 mt-[-2rem] md:mt-2 rounded-full hidden md:block'
+                                    src='/assets/icons/card1-icon.png' alt='safety-icon' width={57} height={42}
+                                />
+                                <p className='text-black p-2 text-[17px] font-medium'>Our riders are vetted. Safety begins with you and everyone in  the society is tasked with their safety and those around them.</p>
+                            </div>
+
+                            <div className='w-[350px] mx-auto md:w-auto bg-white md:bg-transparent shadow-2xl flex flex-col p-4 my-8 rounded-lg hover:scale-105 duration-300 items-center'>
+                                <Image className='w-20 mx-4 mt-[-2rem] md:mt-2 rounded-full hidden md:block'
+                                    src='/assets/icons/card2-icon.png' alt='safety-icon' width={57} height={42}
+                                />
+                                <p className='text-black p-2 text-[17px] font-medium'>Our rides are timely and services easy to access Our are services cheap and help you plan on your transport with ease.</p>
+                            </div>
+
+                            <div className='w-[350px] mx-auto md:w-auto bg-white md:bg-transparent shadow-2xl flex flex-col p-4 my-8 rounded-lg hover:scale-105 duration-300 items-center'>
+                                <Image className='w-20 mx-4 mt-[-2rem] md:mt-2 rounded-full hidden md:block'
+                                    src='/assets/icons/card3-icon.png' alt='safety-icon' width={57} height={42}
+                                />
+                                <p className='text-black p-2 text-[17px] font-medium'>We have services fit for the Kenyan economy and promos fit to client's travel hours. Our technology is up to date and so are our services.</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
